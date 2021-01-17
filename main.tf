cbdef37c4d765ae8c8569d46ea8e277f6c0c7a46
kou164nkn <galahad1005@gmail.com>
2021-01-17 19:19:57 +0900

    Upgrade Terraform 0.14

--- main.tf
+++ main.tf
@@ -2,15 +2,21 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = "ap-northeast-1"
-  version    = ">=2.41.0"
 }
 
 terraform {
-  required_version = ">=0.12.0"
+  required_version = "~>0.14.0"
   backend "s3" {
     region  = "ap-northeast-1"
     bucket  = "kou-terraform-aws-eks"
     key     = "terraform.tfstate.aws.terraform-aws-ecs"
     encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.24.0"
+    }
   }
 }
